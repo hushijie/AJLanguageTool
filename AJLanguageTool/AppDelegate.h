b051e56917f7931189b22e5d649d05f081f0cfd8
hushijie <17744582425@163.com>
2017-09-06 22:37:03 +0800

    Initial Commit

---
+++ AJLanguageTool/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AJLanguageTool
+//
+//  Created by JasonHu on 2017/9/6.
+//  Copyright © 2017年 AJ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AJLanguageTool/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  AJLanguageTool
+//
+//  Created by JasonHu on 2017/9/6.
+//  Copyright © 2017年 AJ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
